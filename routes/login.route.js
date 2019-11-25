157e208598192e43286bde8038e7559090337d8d
kraken47 <inbox.adigunawan@gmail.com>
2019-11-25 17:36:57 +0700

    updated login.routes.js

--- routes/login.route.js
+++ routes/login.route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const Login = require('../actions/login/login')
+const Login = require('../actions/login/login.action')
 
 router.post('/', async (req, res) => {
     try { 
