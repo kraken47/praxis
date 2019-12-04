1ab23cdedd634392b8cf08e9573d409d3b3da829
kraken47 <inbox.adigunawan@gmail.com>
2019-12-04 16:00:51 +0700

    added condition to limit certain method's role_id user to jwt

--- middleware/verify_jwt.middleware.js
+++ middleware/verify_jwt.middleware.js
@@ -6,13 +6,25 @@ module.exports = () => {
     return async (req, res, next) => {
         let token = req.header('Authorization')
         let unauthenticated = {
-            status: 'unauthencated',
+            status: 'unauthenticated',
             message: 'invalid header Token'
         }
+
+        let notAdmin = {
+            status: 'unauthenticated',
+            message: 'you are not admin, you don\'t have previlage'
+        }
+
         if (token) {
         jwt.verify(token, process.env.JWT_SECRET, async (err, data) => {
-            if (err) return res.status(400).json(unauthenticated)
+            console.log(data)
+            if (data.user_permissions === 'User') {
+                if (req.method !== 'GET') {
+                    return res.status(400).json(notAdmin)
+                }
+            }
             return next()
+            
         })
     } else {
         return res.status(400).json(unauthenticated)
